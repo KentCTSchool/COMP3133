f1fbd0ead68ac344beb767362307d08e6736a951
Kent <kentcodetoschool@gmail.com>
2022-03-13 22:30:04 -0400

    Lab 5 TypesScript

---
+++ Lab Exercise/Lab05_101266723/exercise-1/hello-world.ts
@@ -0,0 +1,11 @@
+// var greeter = function (name) {
+//     console.log('Hello' + name);
+// }
+
+// greeter("John Smith");
+
+let greeter = (firstName, lastName) => {
+    console.log(`Hello ${firstName} ${lastName}`)
+}
+
+greeter("John", "Smith");
---
+++ Lab Exercise/Lab05_101266723/exercise-2/customer.js
@@ -0,0 +1,12 @@
+var Customer = /** @class */ (function () {
+    function Customer() {
+    }
+    Customer.prototype.greeter = function () {
+        console.log("Hello ".concat(this.firstName, " ").concat(this.lastName));
+    };
+    return Customer;
+}());
+var customer = new Customer();
+customer.firstName = "John";
+customer.lastName = "Smith";
+customer.greeter();
